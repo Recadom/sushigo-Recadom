9afb9dfda2ac1ab546b088239fe98b3c0de8f5ee
Martin <juskelism@gmail.com>
2018-09-25 15:59:22 -0500

    added a scoring class, made table a hashmap instead of arraylist

--- src/com/company/GameEngine.java
+++ src/com/company/GameEngine.java
@@ -15,7 +15,7 @@ public class GameEngine {
     private List<Player> playerList;
     private List<String> allNames = new ArrayList<>(); //todo reduce scope
     private CardDeck deck = new CardDeck();
-    private ArrayList<List<CardType>> cardsPlayed;
+    private HashMap<String, List<CardType>> cardsOnTable;
     private Map<String, Integer> pointMap;
     private int cardsPerHand, amountOfPlayers;
 
@@ -58,12 +58,12 @@ public class GameEngine {
 
 
         for (int round = 0; round < ROUNDS; round++) {
-            cardsPlayed = new ArrayList<>();
+            cardsOnTable = new HashMap<>();
             playerHands = new CircularLinkedList();
 
-            //deal the initial hands
-            for (int i = 0; i < amountOfPlayers; i++) {
-                cardsPlayed.add(new ArrayList<>());
+            //deal the initial hands  int i = 0; i < amountOfPlayers; i++
+            for (String player : allNames) {
+                cardsOnTable.put(player, new ArrayList<>());
                 playerHands.addNode(generateHand());
             }
 
@@ -72,7 +72,7 @@ public class GameEngine {
 
 
             //repeat until last card has been played
-            for(int cardsPlayedCnt = 0; cardsPlayedCnt < cardsPerHand * amountOfPlayers;) {
+            for(int cardsOnTableCnt = 0; cardsOnTableCnt < cardsPerHand * amountOfPlayers;) {
 
                 List<TurnResult> turnResults = new ArrayList<>();
 
@@ -94,18 +94,18 @@ public class GameEngine {
 
                     //add cards to table
                     if (currentCardPlay.size() == 1 || currentCardPlay.size() == 2) {
-                        cardsPlayed.get(playerNum).addAll(currentCardPlay);
+                        cardsOnTable.get(player.getName()).addAll(currentCardPlay);
                     }
 
-                    turnResults.add(new TurnResult(player.getName(),currentCardPlay, cardsPlayed.get(playerNum)));
+                    turnResults.add(new TurnResult(player.getName(),currentCardPlay, cardsOnTable.get(player.getName())));
 
                     //point the playerHand pointer to the next node; this cycles the player hands
                     playerHand = playerHand.next;
 
                     //count the cards left on the table
-                    cardsPlayedCnt = 0;
-                    for(List<CardType> cards : cardsPlayed) {
-                        cardsPlayedCnt += cards.size();
+                    cardsOnTableCnt = 0;
+                    for(List<CardType> cards : cardsOnTable.values()) {
+                        cardsOnTableCnt += cards.size();
                     }
                 }
 
@@ -114,10 +114,14 @@ public class GameEngine {
                     recPlayer.receiveTurnResults(turnResults);
                 }
 
+                //todo keep track of puddings
+                //todo end game scoring engine, pass pudding + played cards
                 //tally current score todo make sure this is the correct timing for this
-                for (Player player : playerList) {
+                pointMap = Scoring.evaluateBoard(pointMap);//, cardsOnTable);
+
+                /*for (Player player : playerList) {
                     pointMap.put(player.getName(), 10);
-                }
+                }*/
                 playerHand = playerHand.next;
 
             }
---
+++ src/com/company/Scoring.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.Map;
+
+public class Scoring {
+  static Map<String, Integer> evaluateBoard(Map<String, Integer> pointMap) {
+    return pointMap;
+  }
+
+}
