f9d93dec9d56d34df50942fb8cba3ef0c6d48783
Martin <juskelism@gmail.com>
2018-09-26 22:25:18 -0500

    added a few tests

--- src/com/company/tests/com/company/ScoringTest.java
+++ src/com/company/tests/com/company/ScoringTest.java
@@ -2,50 +2,107 @@ package com.company;
 
 import static org.junit.Assert.*;
 
+import com.company.deck.CardDeck;
+import com.company.deck.CardType;
 import com.company.players.RandomPlayer;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.junit.Before;
 import org.junit.Test;
 
 public class ScoringTest {
-  List<Player> playerList = new ArrayList<>();
+  List<Player> playerList;
+  private List<String> allNames;
+  private CardDeck deck;
+  private HashMap<String, List<CardType>> cardsOnTable;
+  private Map<String, Integer> pointMap;
+  private int cardsPerHand, amountOfPlayers;
+  GameEngine gameEngine;
 
   @Before
-  public void setUp() throws Exception {
+  public void setUp() {
+    deck = new CardDeck();
+    allNames = new ArrayList<>();
+    pointMap = new HashMap<>();
+    playerList = new ArrayList<>();
+    cardsOnTable = new HashMap<>();
     playerList.add(new RandomPlayer());
     playerList.add(new RandomPlayer());
     playerList.add(new RandomPlayer());
     playerList.add(new RandomPlayer());
+    gameEngine = new GameEngine(playerList);
+
+      for (Player player : playerList) {
+        allNames.add(player.getName());
+        pointMap.put(player.getName(), 1);
+        //cardsOnTable.put(player.getName(), Arrays.asList(CardType.Wasabi));
+      }
+
+
   }
 
   @Test
-  public void evaluateBoard() {
-    //private List<String> allNames = new ArrayList<>();
-    //assert(null, evaluateBoard();
+  public void evaluateBoardBasicGame() {
+    cardsOnTable = new HashMap<>();
+    for (Player player : playerList) {
+      cardsOnTable.put(player.getName(), Arrays.asList(CardType.Wasabi));
+    }
+    assertEquals(pointMap, Scoring.evaluateBoard(cardsOnTable, allNames, 1));
+  }
+
+  @Test
+  public void tempuraScoringTest() {
+    cardsOnTable = new HashMap<>();
+    for (Player player : playerList) {
+      cardsOnTable.put(player.getName(), Arrays.asList(CardType.Tempura));
+    }
+    assertEquals(pointMap, Scoring.evaluateBoard(cardsOnTable, allNames, 1));
+  }
+
+  @Test
+  public void MakiScoringTest() {
+    cardsOnTable = new HashMap<>();
+    for (Player player : playerList) {
+      cardsOnTable.put(player.getName(), Arrays.asList(CardType.MakiRollOne));
+    }
+    assertEquals(pointMap, Scoring.evaluateBoard(cardsOnTable, allNames, 1));
+  }
+
+  @Test
+  public void WasabiScoringTest() {
+    cardsOnTable = new HashMap<>();
+    for (Player player : playerList) {
+      cardsOnTable.put(player.getName(), Arrays.asList(CardType.MakiRollOne));
+      cardsOnTable.put(player.getName(), Arrays.asList(CardType.EggNigiri));
+    }
+    assertEquals(pointMap, Scoring.evaluateBoard(cardsOnTable, allNames, 1));
   }
 
   @Test
   public void countMaki() {
+    Scoring.countMaki(allNames);
   }
 
   @Test
   public void countTempura() {
+    Scoring.countTempura("Player 1");
   }
 
   @Test
   public void nigiriWasabi() {
-  }
-
-  @Test
-  public void countSashimi() {
+    Scoring.nigiriWasabi(allNames);
   }
 
   @Test
   public void countDumplings() {
+    Scoring.countDumplings("Player 1");
   }
 
   @Test
   public void countPudding() {
+    Scoring.countPudding(allNames);
   }
 }
